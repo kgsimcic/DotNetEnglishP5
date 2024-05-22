b09265416f6f64f601e420058ee7d851457d204f
Karolina Simcic <kgsimcic@gmail.com>
2024-05-22 14:20:53 -0400

    migrated data and configured azure database

--- CarHub/Program.cs
+++ CarHub/Program.cs
@@ -7,7 +7,7 @@ using CarHub.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("AzureConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("AzureConnection") ?? throw new InvalidOperationException("Connection string 'AzureConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
